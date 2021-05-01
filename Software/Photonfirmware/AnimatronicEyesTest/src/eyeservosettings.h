618572fe87824501dd68a4a44206ea490bd99925
Jim Schrempp <github1@jimschrempp.com>
2021-04-30 17:21:02 -0700

    v1.2 idle eyes

---
+++ Software/Photonfirmware/AnimatronicEyesTest/src/eyeservosettings.h
@@ -0,0 +1,25 @@
+// Servo Positions
+// Use the calibration test code to figure the correct values for each.
+// THE CALIBRATION TEST WILL PRINT A SET OF DEFINES THAT YOU CAN COPY
+// AND PASTE OVER THE ONES BELOW. 
+// -------------------------------------------------------------------
+// -------------------------------------------------------------------
+
+    #define X_POS_MID 479
+    #define X_POS_LEFT_OFFSET 115
+    #define X_POS_RIGHT_OFFSET -128
+
+    #define Y_POS_MID 343
+    #define Y_POS_UP_OFFSET 93
+    #define Y_POS_DOWN_OFFSET -72
+
+    #define LEFT_UPPER_CLOSED 473
+    #define LEFT_UPPER_OPEN 260
+
+    #define LEFT_LOWER_CLOSED 317
+    #define LEFT_LOWER_OPEN 498
+
+    #define RIGHT_UPPER_OFFSET 760
+    #define RIGHT_LOWER_OFFSET 700
+// -------------------------------------------------------------------
+// -------------------------------------------------------------------
